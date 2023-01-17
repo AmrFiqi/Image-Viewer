8c45aab298cb2de1e8eeaff98b3df4ca9179de97
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-01-17 13:06:08 +0200

    Loading image from app bundle done in the background async function using GCD

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -18,23 +18,28 @@ class ViewController: UITableViewController {
         
         let fm = FileManager.default
         let path =  Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
         
-        for item in items {
-            if item.hasPrefix("nssl"){
-                pictures.append(item)
+        DispatchQueue.global(qos: .utility).async {
+            [weak self] in
+            let items = try! fm.contentsOfDirectory(atPath: path)
+            for item in items {
+                if item.hasPrefix("nssl"){
+                    self?.pictures.append(item)
+                }
             }
-            
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
         }
-        print(pictures)
+        
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        
         cell.textLabel?.text = pictures.sorted()[indexPath.row]
+        
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
